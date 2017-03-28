24ebfd2545994e948d90674d81db04785042ba8a
Christoffer <chrlov91@gmail.com>
2017-03-28 21:49:32 +0200

    Movement Code

--- Causality/Assets/Scripts/Camera/Camera_Follow.cs
+++ Causality/Assets/Scripts/Camera/Camera_Follow.cs
@@ -33,8 +33,8 @@ public class Camera_Follow : MonoBehaviour
     {
         transform.position = Player.transform.position + currentOffset;
 
-        float movementX = Input.GetAxis("Horizontal") * angularSpeed * Time.deltaTime;
-        float movementY = Input.GetAxis("Vertical") * angularSpeed * Time.deltaTime;
+        float movementX = Input.GetAxis("HorizontalTurn") * angularSpeed * Time.deltaTime;
+        float movementY = Input.GetAxis("VerticalTurn") * angularSpeed * Time.deltaTime;
         if (!Mathf.Approximately(movementX, 0f))
         {
             transform.RotateAround(Player.transform.position, Vector3.up, movementX);
--- Causality/Assets/Scripts/Player/Movement.cs
+++ Causality/Assets/Scripts/Player/Movement.cs
@@ -4,13 +4,59 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
 
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    private Vector3 moveDirection = Vector3.zero;
+    private bool Roll;
+    private bool Charge;
+    private float RollTimer;
+    private float RollTime = 1f;
+    private float Rollspeed = 15f;
+    private float CurrentSpeed = 0;
+    private float RollCD;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        CharacterController controller = GetComponent<CharacterController>();
+        CurrentSpeed = speed;
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= CurrentSpeed;
+
+            if (RollCD <= 0)
+            {
+                if (Input.GetButton("A Button"))
+                {
+                    Roll = true;
+                    RollCD = 5;
+                }
+            }
+            else
+            {
+                RollCD -= Time.fixedDeltaTime;
+            }
+        }
+
+        if (Roll)
+        {
+            moveDirection = transform.forward / 5f * Rollspeed;
+            CurrentSpeed = Rollspeed;
+            RollTimer += Time.fixedDeltaTime;
+            if (RollTime < RollTimer)
+            {
+                Roll = false;
+                RollTimer = 0;
+            }
+        }
+
+        moveDirection.y -= gravity * Time.fixedDeltaTime;
+        controller.Move(moveDirection * Time.fixedDeltaTime);
+    }
 }
