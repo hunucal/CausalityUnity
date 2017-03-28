81d2c43de227fe7026769e2ee918201d2b20ecf9
joni <nujabes@live.se>
2017-03-28 21:24:27 +0200

    no message

--- Causality/Assets/Scripts/Camera/Camera_Follow.cs
+++ Causality/Assets/Scripts/Camera/Camera_Follow.cs
@@ -1,38 +1,71 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Camera_Follow : MonoBehaviour {
+using System;
+public class Camera_Follow : MonoBehaviour
+{
     private GameObject Player;
-    public Vector3 directionVector;
-    public Vector3 velocityVector;
     public float distanceFromTarget;
-
-    private float heightDampening;
-    private float rotationDampening;
-    private float rotationCurrent;
-    private float rotationWanted;
+    public float height;
+    public float heightDampening;
+    public float rotationDampening;
+    public float lookSpeed;
+    public float angularSpeed;
+    private float rotationCurrentAngle;
+    private float rotationWantedAngle;
     private float heightWanted;
     private float heightCurrent;
 
+    [SerializeField]
+    [HideInInspector]
+    private Vector3 initialOffset;
+    private Vector3 currentOffset;
+    // Use this for initialization
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [ContextMenu("Set Current Offset")]
+    private void SetCurrentOffset()
+    {
+        if (Player == null)
+        {
+            return;
+        }
+
+        initialOffset = transform.position - Player.transform.position;
+    }
+
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        if (Player == null)
+        {
+            Debug.LogError("Assign a target for the camera in Unity's inspector");
+        }
+
+        currentOffset = initialOffset;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
     void LateUpdate()
     {
-        if (!gameObject)
-            return;
+        transform.position = Player.transform.position + currentOffset;
 
-        rotationWanted = Player.transform.eulerAngles.y;
-        heightWanted = Player.transform.position.y + heightCurrent;
-        rotationCurrent = Player.transform.eulerAngles.y;
-        heightCurrent = transform.position.y;
+        float movementX = Input.GetAxis("Horizontal") * angularSpeed * Time.deltaTime;
+        float movementY = Input.GetAxis("Vertical") * angularSpeed * Time.deltaTime;
+        if (!Mathf.Approximately(movementX, 0f))
+        {
+            transform.RotateAround(Player.transform.position, Vector3.up, movementX);
+            currentOffset = transform.position - Player.transform.position;
+        }
+        else if (!Mathf.Approximately(movementY, 0f))
+        {
+            transform.RotateAround(Player.transform.position, Vector3.right, movementY);
+            currentOffset = transform.position - Player.transform.position;
+        }
+
     }
+
 }
