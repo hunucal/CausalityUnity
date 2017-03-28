3dc656bd72deffcd42d1855cc0bbefce1399b7cf
joni <nujabes@live.se>
2017-03-28 19:11:38 +0200

    no message

--- Causality/Assets/Scripts/Camera/Camera_Follow.cs
+++ Causality/Assets/Scripts/Camera/Camera_Follow.cs
@@ -3,6 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Camera_Follow : MonoBehaviour {
+    private GameObject Player;
+    public Vector3 directionVector;
+    public Vector3 velocityVector;
+    public float distanceFromTarget;
+
+    private float heightDampening;
+    private float rotationDampening;
+    private float rotationCurrent;
+    private float rotationWanted;
+    private float heightWanted;
+    private float heightCurrent;
+
 
 	// Use this for initialization
 	void Start () {
@@ -13,4 +25,14 @@ public class Camera_Follow : MonoBehaviour {
 	void Update () {
 		
 	}
+    void LateUpdate()
+    {
+        if (!gameObject)
+            return;
+
+        rotationWanted = Player.transform.eulerAngles.y;
+        heightWanted = Player.transform.position.y + heightCurrent;
+        rotationCurrent = Player.transform.eulerAngles.y;
+        heightCurrent = transform.position.y;
+    }
 }
