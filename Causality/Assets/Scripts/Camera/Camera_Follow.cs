2a83fa5e5636abde138e8cdab49a76e42e13b22b
Axelinien Eriksson <Axelinien Eriksson>
2017-03-30 12:02:07 +0200

    Camera almost Done. hope for no error?

--- Causality/Assets/Scripts/Camera/Camera_Follow.cs
+++ Causality/Assets/Scripts/Camera/Camera_Follow.cs
@@ -11,13 +11,22 @@ public class Camera_Follow : MonoBehaviour
     [SerializeField]
     private float smooth;
     [SerializeField]
-    private Transform follow;
+    private Transform followXform;
+    [SerializeField]
+    private Vector3 offset = new Vector3(0f, 1.5f, 0f);
 
+    // Private global only
+    private Vector3 lookDir;
     private Vector3 targetPosition;
+
+    // Smoothing and damping
+    private Vector3 velocityCamSmooth = Vector3.zero;
+    [SerializeField]
+    private float canSmoothDampTime = 0.1f;
 
     void start()
     {
-        follow = GameObject.FindWithTag("Player").transform;
+        followXform = GameObject.FindWithTag("Player").transform;
     }
 
 
@@ -33,16 +42,32 @@ public class Camera_Follow : MonoBehaviour
 
     void LateUpdate()
     {
-        // Setting the target to be the correct offset from the hovercraft
-        targetPosition = follow.position + follow.up * distanceUp - follow.forward * distanceAway;
-        Debug.DrawRay(follow.position, Vector3.up * distanceUp, Color.red);
-        Debug.DrawRay(follow.position, -1f * follow.forward * distanceAway, Color.blue);
-        Debug.DrawLine(follow.position, targetPosition, Color.magenta);
+        Vector3 characterOffset = followXform.position + offset;
 
-        // Making a smooth transistion between it´s current position and the position it wants to be in
-        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smooth);
+        // Calculate diraction from camera to player, kill Y, and normalize to give a valid direction with unit magnitude
+        lookDir = characterOffset - this.transform.position;
+        lookDir.y = 0;
+        lookDir.Normalize();
+        Debug.DrawRay(this.transform.position, lookDir, Color.green);
+
+
+
+        // Setting the target to be the correct offset from the hovercraft
+        targetPosition = followXform.position + followXform.up * distanceUp - followXform.forward * distanceAway;
+       // Debug.DrawRay(follow.position, Vector3.up * distanceUp, Color.red);
+       // Debug.DrawRay(follow.position, -1f * follow.forward * distanceAway, Color.blue);
+        Debug.DrawLine(followXform.position, targetPosition, Color.magenta);
+
+        // Making a smooth transistion between its current position and the position it wants to be in
+        smoothPosition(this.transform.position, targetPosition);
 
         // make sure the camera is looking the right way
-        transform.LookAt(follow);
+        transform.LookAt(followXform);
+    }
+
+    private void smoothPosition(Vector3 fromPos, Vector3 toPos)
+    {
+        // Makng a smooth transition between cameras current position to the position it wants to be in
+        this.transform.position = Vector3.SmoothDamp(fromPos, toPos, ref velocityCamSmooth, canSmoothDampTime);
     }
 }
--- Causality/Assets/Scripts/Player/Movement.cs
+++ Causality/Assets/Scripts/Player/Movement.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
 
-    public float Movement_Speed = 6.0f;
+    public float speed = 6.0f;
     public float jumpSpeed = 8.0f;
     public float gravity = 20.0f;
     private Vector3 moveDirection = Vector3.zero;
-
     private bool Roll;
     private bool Charge;
     private float RollTimer;
@@ -16,90 +15,48 @@ public class Movement : MonoBehaviour {
     private float Rollspeed = 15f;
     private float CurrentSpeed = 0;
     private float RollCD;
-
-    //private Rigidbody rb;
-    //CharacterController controller;
-
-    //Stick directions
-    private float verticalForce;
-    private float horizontalForce;
-
-    //Rotation variables
-    private Vector3 playerAim;
-    private Vector3 rotationDifVec;
-    private float newAngle;
-
-    // Use this for initialization
-    void Start () {
-        //rb = GetComponent<Rigidbody>();
+	// Use this for initialization
+	void Start () {
+        
     }
 	
 	// Update is called once per frame
 	void LateUpdate () {
-
         CharacterController controller = GetComponent<CharacterController>();
-        
-        CurrentSpeed = Movement_Speed;
+        CurrentSpeed = speed;
         if (controller.isGrounded)
         {
-            MovementAnalog();
-        }
-        //    if (RollCD <= 0)
-        //    {
-        //        if (Input.GetButton("A Button"))
-        //        {
-        //            Roll = true;
-        //            RollCD = 5;
-        //        }
-        //    }
-        //    else
-        //    {
-        //        RollCD -= Time.fixedDeltaTime;
-        //    }
-        //}
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, -Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= CurrentSpeed;
 
-        //if (Roll)
-        //{
-        //    moveDirection = transform.forward / 5f * Rollspeed;
-        //    CurrentSpeed = Rollspeed;
-        //    RollTimer += Time.fixedDeltaTime;
-        //    if (RollTime < RollTimer)
-        //    {
-        //        Roll = false;
-        //        RollTimer = 0;
-        //    }
-        //}
+            if (RollCD <= 0)
+            {
+                if (Input.GetButton("A Button"))
+                {
+                    Roll = true;
+                    RollCD = 5;
+                }
+            }
+            else
+            {
+                RollCD -= Time.fixedDeltaTime;
+            }
+        }
+
+        if (Roll)
+        {
+            moveDirection = transform.forward / 5f * Rollspeed;
+            CurrentSpeed = Rollspeed;
+            RollTimer += Time.fixedDeltaTime;
+            if (RollTime < RollTimer)
+            {
+                Roll = false;
+                RollTimer = 0;
+            }
+        }
 
         moveDirection.y -= gravity * Time.fixedDeltaTime;
         controller.Move(moveDirection * Time.fixedDeltaTime);
-    }
-
-    void MovementAnalog()
-    {
-        horizontalForce = Input.GetAxis("Horizontal");
-        verticalForce = Input.GetAxis("Vertical");
-
-        //Move
-        if (verticalForce != 0 || horizontalForce != 0)
-        {
-            Move(horizontalForce, verticalForce);
-        }
-    }
-
-    void Move(float x, float z)
-    {
-        //Rotate towards stick direction
-        Rotate(x, -z);
-        //Move in new direction
-        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, -Input.GetAxis("Vertical"));
-        moveDirection *= CurrentSpeed;
-    }
-
-    void Rotate(float x, float z)
-    {
-        newAngle = Vector3.Angle(Vector3.forward, new Vector3(x, 0, z)); //Gets global angle
-        if (x < 0) { newAngle = -newAngle; } //flip angle if left side
-        transform.localEulerAngles = new Vector3(0f, newAngle, 0f);
-        //TODO normalize and smooth
     }
 }
