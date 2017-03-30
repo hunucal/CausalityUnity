1a297319d5700d9e66966dcb3afd65e427355769
Axelinien Eriksson <Axelinien Eriksson>
2017-03-30 10:28:30 +0200

    CameraStartFix

--- Causality/Assets/Scripts/Camera/Camera_Follow.cs
+++ Causality/Assets/Scripts/Camera/Camera_Follow.cs
@@ -4,66 +4,45 @@ using UnityEngine;
 using System;
 public class Camera_Follow : MonoBehaviour
 {
-    private GameObject Player;
-    public float distanceFromTarget;
-    public float height;  
-    public float lookSpeed;
-    public float angularSpeed;
-    public Vector3 initialOffset;
-    public float minY;
-    public float maxY;
+    [SerializeField]
+    private float distanceAway;
+    [SerializeField]
+    private float distanceUp;
+    [SerializeField]
+    private float smooth;
+    [SerializeField]
+    private Transform follow;
 
-    private float angle;
-    private bool moveX, moveY;
-    private Vector3 currentOffset;
-    // Use this for initialization
-    void Start()
+    private Vector3 targetPosition;
+
+    void start()
     {
-        angle = 0f;
-        moveX = false;
-        moveY = false;
-        Player = GameObject.FindGameObjectWithTag("Player");
-        if (Player == null)
-        {
-            Debug.LogError("Assign a target for the camera in Unity's inspector");
-        }
-
-        currentOffset = initialOffset;
+        follow = GameObject.FindWithTag("Player").transform;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void update()
     {
 
     }
+
+    void OnDrawGizmos()
+    {
+
+    }
+
     void LateUpdate()
     {
-        transform.position = Player.transform.position + currentOffset;
+        // Setting the target to be the correct offset from the hovercraft
+        targetPosition = follow.position + follow.up * distanceUp - follow.forward * distanceAway;
+        Debug.DrawRay(follow.position, Vector3.up * distanceUp, Color.red);
+        Debug.DrawRay(follow.position, -1f * follow.forward * distanceAway, Color.blue);
+        Debug.DrawLine(follow.position, targetPosition, Color.magenta);
 
-        float movementX = Input.GetAxis("HorizontalTurn") * angularSpeed * Time.deltaTime;
-        float movementY = Input.GetAxis("VerticalTurn") * angularSpeed * Time.deltaTime;
-        if (!moveY)
-        {
-            if (!Mathf.Approximately(movementX, 0f))
-            {
-                transform.RotateAround(Player.transform.position, Vector3.up, movementX);
-                currentOffset = transform.position - Player.transform.position;
-                moveX = true;
-            }
-        }
-        if (Mathf.Approximately(movementX, 0f))
-            moveX = false;
-        if (Mathf.Approximately(movementY, 0f))
-            moveY = false;
-        if (!moveX)
-        {
-            if (!Mathf.Approximately(movementY, 0f))
-            {
-                transform.RotateAround(Player.transform.position, Vector3.right, movementY);
-                currentOffset = transform.position - Player.transform.position;
-                moveY = true;
-            }
-        }
-        transform.LookAt(Player.transform);
+        // Making a smooth transistion between it´s current position and the position it wants to be in
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smooth);
+
+        // make sure the camera is looking the right way
+        transform.LookAt(follow);
     }
 }
