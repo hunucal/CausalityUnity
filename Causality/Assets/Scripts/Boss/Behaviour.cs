f009cc70605dfa5d6da752ad3ada5dd7b4f05555
joni <nujabes@live.se>
2017-03-30 15:35:00 +0200

    no message

--- Causality/Assets/Scripts/Boss/Behaviour.cs
+++ Causality/Assets/Scripts/Boss/Behaviour.cs
@@ -1,16 +1,82 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Analytics;
 
 public class Behaviour : MonoBehaviour {
     private StateMachine finiteStateMachine;
+
+    private moveState bossMovingState;
+    private IdleState bossIdleState;
+
+    private State lastState;
+
+    private bool initState;
+    private bool stateChange;
+    private bool runState;
+
+    [SerializeField]private float bossMoveSpeed;
+    [SerializeField]private float bossRotationSpeed;
+
+
 	// Use this for initialization
 	void Start () {
+        //state machine
         finiteStateMachine = new StateMachine();
+
+        //states
+        bossMovingState = new moveState();
+        bossIdleState = new IdleState();
+
+
+        //bools
+        initState = true;
+        stateChange = false;
+        runState = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if(initState)
+        {
+            //move state data
+            bossMovingState.setMovementDate(bossMoveSpeed, bossRotationSpeed);
+            bossMovingState.Init();
+
+            //idle state data
+            bossIdleState.setIdleDate(6f);
+            bossIdleState.Init();
+
+            //logic variables
+            stateChange = true;
+            initState = false;
+        }
+        if (stateChange)
+        {
+            if (!UnityEngine.UnassignedReferenceException.ReferenceEquals(lastState, null)) // check if last state does not equal null
+            {
+                if (lastState == bossIdleState)
+                    finiteStateMachine.ChangeState(bossMovingState);
+                else if (lastState == bossMovingState)
+                    finiteStateMachine.ChangeState(bossIdleState);
+            }
+            else if (UnityEngine.UnassignedReferenceException.ReferenceEquals(lastState, null)) // if laststate is null set state to idle
+            {
+                finiteStateMachine.ChangeState(bossIdleState);
+            }
+            runState = true;
+            stateChange = false;
+        }
+        if(runState)
+        {
+            if(!finiteStateMachine.RunState())
+            {
+                lastState = finiteStateMachine.GetCurrentState();
+                runState = false;
+                initState = true;
+                stateChange = false;
+            }
+        }
+
 	}
 }
