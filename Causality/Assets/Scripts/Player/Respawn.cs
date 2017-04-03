89a25f582725e27c2cbb4c0ce2ed929cbe3297f6
Robin <Robinberneby@hotmail.com>
2017-04-03 14:04:48 +0200

    Test för att få health att fungera

--- Causality/Assets/Scripts/Player/Respawn.cs
+++ Causality/Assets/Scripts/Player/Respawn.cs
@@ -5,29 +5,44 @@ using UnityEngine.UI;
 
 public class Respawn : MonoBehaviour {
 
-    public float Health;
+    //public float Health;
     public Text DeadText;
+
+    [SerializeField]
+    private Stat Health;
+
+    public float Timer = 0;
 
 	// Use this for initialization
 	void Start () {
-        Health = 1;
         DeadText.text = "";
 		
 	}
+
+    private void Awake()
+    {
+        Health.Initialize();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if(Input.GetKeyDown(KeyCode.A))
+
+        if(Input.GetKey(KeyCode.A))
         {
-            Health -= Time.deltaTime;
+           Timer += Time.deltaTime;
+            if (Timer < 5)
+            {
+                Health.CurrentValHealth += 5 * Time.deltaTime;
+            }
+            if(Timer >= 20)
+            {
+                Timer = 0;
+            }
+            
         }
 
-        if (Health <= 0)
-        {
             transform.position = new Vector3(5.0f, 0.5799999f, 1.98f);
             DeadText.text = ("You are dead");
-            Health = 1;
-        }
 		
 	}
 }
