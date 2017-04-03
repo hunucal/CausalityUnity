b1a84c71c46edda8b71380405df6b7112ed05243
Axelinien Eriksson <Axelinien Eriksson>
2017-04-03 13:28:15 +0200

    2:nd

---
+++ Causality/Assets/Scripts/Player/Respawn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Respawn : MonoBehaviour {
+
+    public float Health;
+    public Text DeadText;
+
+	// Use this for initialization
+	void Start () {
+        Health = 1;
+        DeadText.text = "";
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            Health -= Time.deltaTime;
+        }
+
+        if (Health <= 0)
+        {
+            transform.position = new Vector3(5.0f, 0.5799999f, 1.98f);
+            DeadText.text = ("You are dead");
+            Health = 1;
+        }
+		
+	}
+}
