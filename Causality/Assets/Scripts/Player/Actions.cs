6751b279f50be786ba0ba0e9c59af1750846a6ee
Christoffer <chrlov91@gmail.com>
2017-04-03 10:32:54 +0200

    no message

--- Causality/Assets/Scripts/Player/Actions.cs
+++ Causality/Assets/Scripts/Player/Actions.cs
@@ -3,64 +3,98 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Actions : MonoBehaviour {
+    
+    //Player Attributes
+    private float defence;
+    private float strenght;
+    private float agility;
+    private float damage;
+    private float stamina;
+    
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start () {
+        
+        
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
         Inputs();
+
+        
 	}
 
     void Inputs()
     {
         if(Input.GetButtonDown("A Button"))
         { 
+            //Select
         }
         if (Input.GetButtonDown("X Button"))
         {
+            //Sprint/Dash
         }
         if (Input.GetButtonDown("B Button"))
         {
+            //Roll
         }
         if (Input.GetButtonDown("Y Button"))
         {
+            //No Action yet.
         }
         if (Input.GetButtonDown("RB Button"))
         {
+            //Heavy Attack
+            HeavyAttack();
+        }
+        if (Input.GetButtonDown("LB Button"))
+        {
+            //No Action yet.
         }
         if (Input.GetButtonDown("View Button"))
         {
+            //No Action yet.
         }
         if (Input.GetButtonDown("Menu Button"))
         {
+            //No Action yet.
         }
-        //if (Input.GetAxis("RT Button"))
-        //{
-        //   FastAttack();
-        //}
+        if (Input.GetAxis("RT Button") != 0)
+        {
+            //Fast Attack
+            FastAttack();
+        }
+        if (Input.GetAxis("LT Button") != 0)
+        {
+            //No Action yet.
+        }
     }
 
     void FastAttack()
     {
         //Fast attack
+        //  Animation.Instantiate<FastAttack>();
+
+
+        //Damage(10);
     }
 
-    void HardAttack()
+    void HeavyAttack()
     {
-        //Hard attack
+        //Heavy attack
+        //Damage(20);
     }
 
     void Block()
     {
         //Block with weapon
+
     }
 
     void ShieldBlock()
     {
-        //Block with shield
+        //Block with shield with bash
     }
 
     void Select()
@@ -71,10 +105,21 @@ public class Actions : MonoBehaviour {
     void Roll()
     {
         //Code Roll in movement use here?
+
     }
 
     void Dash()
     {
         //Code Dash in movement use here?
+
+    }
+    void DamageGiven(float x, float edef)
+    {
+        //Calculate Damage
+        damage = x + strenght * agility / edef;
+    }
+    void DamageTaken(float x, float estr, float eagi)
+    {
+        damage = x + estr * eagi / defence;
     }
 }
